152dfee4db62f5675e31f0a708cb15a3aab13cb4
Jake Zeal <jake@jakezeal.com>
2017-03-22 10:43:17 -0700

    update file structure and add signUpVC

---
+++ whale-ios/whale-ios/UI/Scenes/Authentication/SignUpViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpViewController.swift
+//  whale-ios
+//
+//  Created by Jake on 3/22/17.
+//  Copyright © 2017 Jake. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
